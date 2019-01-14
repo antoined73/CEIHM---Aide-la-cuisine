dba1dfad7700a297037874d59624b92dba7c1e1b
Kariamoss <jehanmilleret@gmail.com>
2019-01-14 09:46:21 +0100

    Function to control video with gestual / vocal commands

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -1,7 +1,14 @@
 <template>
 
     <section class="video-player">
-        <youtube :video-id="videoId" :player-vars="playerVars" :resize="true" :fitParent="true" ref="youtube" @ready="playVideo" ></youtube>
+        <youtube :video-id="videoId" :player-vars="playerVars" :resize="true" :fitParent="true" ref="youtube" @ready="playVideo" @playing="playingVideo" @paused="pausedVideo"></youtube>
+        <div class="flex-column">
+            <a class="button is-primary" @click="play()" v-if="!playing">Play</a>
+            <a class="button is-primary" @click="pause()" v-if="playing">Pause</a>
+            <a class="button is-secondary" @click="forward()">Avancer</a>
+            <a class="button is-secondary" @click="backward()">Reculer</a>
+        </div>
+        
     </section>
     
 </template>
@@ -16,12 +23,40 @@ export default {
             playerVars: {
                 autoplay: 0,
                 origin: "localhost:8080"
-            }
+            },
+            playing: false
         }
     },
     methods: {
         playVideo() {
-            this.$refs.youtube.player.playVideo()
+            this.player.playVideo()
+        },
+        playingVideo() {
+            this.playing = true;
+        },
+        pausedVideo() {
+            this.playing = false;
+        },
+        play() {
+            this.player.playVideo();
+        },
+        pause() {
+            this.player.pauseVideo();
+        },
+        forward() {
+            this.player.getCurrentTime().then( value => {
+                this.player.seekTo(value + 10, true);
+            });
+        },
+        backward() {
+            this.player.getCurrentTime().then( value => {
+                this.player.seekTo(value - 10, true);
+            });
+        }
+    },
+    computed: {
+        player() {
+            return this.$refs.youtube.player
         }
     }
 

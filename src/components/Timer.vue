14a8bb50d677d2033f050db2d65d2f9b52fb21fe
Alexandre HILTCHER <alex.hiltcher@gmail.com>
2019-01-17 10:33:09 +0100

    setup timer service + start component

---
+++ src/components/Timer.vue
@@ -0,0 +1,107 @@
+<!--https://scotch.io/bar-talk/build-a-pomodoro-timer-with-vuejs-solution-to-code-challenge-6-->
+<template>
+    <!-- our template -->
+    <div>
+        <div id="timer">
+            <span id="minutes">{{timer.getMinutes()}}</span>
+            <span id="middle">:</span>
+            <span id="seconds">{{timer.getSeconds()}}</span>
+        </div>
+
+        <div id="buttons">
+
+            <!--  START BUTTON    -->
+            <button
+                    class="button is-dark is-large"
+                    id="start"
+                    @click="start">
+                <i class="far fa-play-circle"></i>
+            </button>
+
+            <!--   PAUSE BUTTON   -->
+            <button
+                    class="button is-dark is-large"
+                    id="stop"
+                    @click="stop">
+                <i class="far fa-pause-circle"></i>
+            </button>
+
+            <!--RESET BUTTON   -->
+            <button
+                    class="button is-dark is-large"
+                    id="reset"
+                    @click="reset">
+                <i class="fas fa-undo"></i>
+            </button>
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+
+    import Timer from '../services/timer';
+    import VocalRecognition from '../services/vocal-regognition';
+
+    VocalRecognition.initContext({
+        'Démarer': () => {
+            start();
+        },
+        'Stop': () => {
+            Timer.stop();
+        },
+        'Recommencer': () => {
+            Timer.clear();
+        }
+    });
+
+    Timer.setTick(() => {
+
+    });
+
+    Timer.setEnd(() => {
+
+    });
+
+    function start() {
+        if (Timer.isRunning()) {
+            Timer.resume()
+        } else {
+            Timer.setDuration(50);
+            Timer.start();
+        }
+    }
+
+
+    export default {
+        name: 'TimerView',
+        props: {},
+
+        data() {
+            return {
+                minutes: Timer.getMinutes(),
+                seconds: Timer.getSeconds(),
+                timer: Timer
+            }
+        },
+        methods: {
+
+            recipeClicked() {
+                this.$router.push('recipe/' + this.recipe.id);
+            }, start() {
+                start();
+            },
+
+            stop() {
+                Timer.stop();
+            },
+            reset() {
+                Timer.clear();
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
--- src/services/router.js
+++ src/services/router.js
@@ -1,25 +1,26 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 // Views components
-import AccueilView from './../components/views/AccueilView.vue'
-import RecipeView from './../components/views/RecipeView.vue'
-import NotFoundView from './../components/views/NotFoundView.vue'
-import VideoView from './../components/views/VideoView.vue'
+import AccueilView from './../components/views/AccueilView.vue';
+import RecipeView from './../components/views/RecipeView.vue';
+import NotFoundView from './../components/views/NotFoundView.vue';
+import VideoView from './../components/views/VideoView.vue';
+import TimerView from '../components/Timer';
 
 Vue.use(VueRouter);
 
 const routes = [
-    { path: '/', redirect: '/home' },
-    { path: '/home', component: AccueilView },
-    { path: '/recipe/:id', component: RecipeView },
-    { path: '/recipeVideo/:id', component: VideoView},
-    { path: '*', component: NotFoundView }
-  ]
-  
+    {path: '/', redirect: '/home'},
+    {path: '/home', component: AccueilView},
+    {path: '/recipe/:id', component: RecipeView},
+    {path: '/recipeVideo/:id', component: VideoView},
+    {path: '*', component: NotFoundView},
+    {path: '/timer', component: TimerView}
+];
+
 const router = new VueRouter({
     mode: "history",
-    routes : routes
-})
+    routes: routes
+});
 
 export default router;
